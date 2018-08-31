b7a448764c6342dc289a6e4a65f533ecb5e1ecd2
faizal007 <42862461+faizal007@users.noreply.github.com>
2018-08-31 14:38:16 +0530

    Create positive.py

---
+++ positive.py
@@ -0,0 +1,7 @@
+number_check=int(raw_input("Enter the number:"))
+if(number_check>0):
+  print("The number entered is positive")
+elif(number_check<0):
+  print("The number entered is negative ")
+else:
+  print("the number is 0") 
