8ed1c727d0b16717c6b69801b4b3f601b1c37980
faizal007 <42862461+faizal007@users.noreply.github.com>
2019-02-07 10:50:09 +0530

    Update largest3.py

--- largest3.py
+++ largest3.py
@@ -3,7 +3,7 @@ num21=int(raw_input())
 num31=int(raw_input())
 if (num11 >= num21) and (num11 >= num31):
    print(num11)
-elif (num21 >= num11) and (num21 >= num31):
+elif (num21 >= num31):
    print(num21)
 else:
    print(num31)
